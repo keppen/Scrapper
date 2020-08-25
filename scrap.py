13cc0c39fc26ad131926f507bb334123b1615af1
Maksymilian Szatko <szatko.maksymilian@gmail.com>
2020-08-26 00:51:42 +0200

    Getting to know with bs4.

--- scrap.py
+++ scrap.py
@@ -6,15 +6,47 @@ import time
 
 print ('done')
 
-url = 'https://www.mobygames.com/browse/games/offset,0/so,0a/list-games/'
+url1 = "https://chem.libretexts.org/Bookshelves/Ancillary_Materials/Reference/Reference_Tables/Atomic_and_Molecular_Properties/A3%3A_Covalent_Radii"
 
-response = requests.get(url)
-print (response.url, '\n', response)
+response = requests.get(url1)
 
-soup = bs4.BeautifulSoup(response.text, 'lxml')
+soup1 = bs4.BeautifulSoup(response.text, 'lxml')
 
-print (len(soup.tbody))
+url2 = 'https://chem.libretexts.org/Bookshelves/Ancillary_Materials/Reference/Reference_Tables/Atomic_and_Molecular_Properties/A2%3A_Electronegativity_Values'
+
+'''
+response = requests.get(url2)
+
+soup2 = bs4.BeautifulSoup(response.text, 'lxml')
+
+url3 = 'https://chem.libretexts.org/Bookshelves/Ancillary_Materials/Reference/Reference_Tables/Atomic_and_Molecular_Properties/A4%3A_Atomic_Weights_and_Isotope_Composition'
+
+response = requests.get(url3)
+
+soup3 = bs4.BeautifulSoup(response.text, 'lxml')
+
+url4 = 'https://en.wikipedia.org/wiki/Ionic_radius'
+
+response = requests.get(url4)
+
+soup4 = bs4.BeautifulSoup(response.text, 'lxml')
+'''
+
+properties = {}
+
+search = soup1.td
+def check(soup):
+    count = 0
+    for element in soup.next_elements:
+        print (element)
+        count += 1
+        if count == 25:
+            break
+
+def scrap (soup):
+    if soup.name = 'td':
+        if soup.name
 
 
-for href in soup.tbody:
-    print (href)
+check(search)
+print (properties)
